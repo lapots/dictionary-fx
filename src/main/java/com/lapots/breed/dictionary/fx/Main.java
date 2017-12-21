c22365bc4978cd8daf864bd35cb9026bb211ae79
lapots <sebastrident@gmail.com>
2017-12-21 12:14:00 +0300

    JavaFX tutorial 3

--- src/main/java/com/lapots/breed/dictionary/fx/Main.java
+++ src/main/java/com/lapots/breed/dictionary/fx/Main.java
@@ -27,6 +27,53 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
     }
 
+    private void tutorial3(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Welcome");
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+
+        Text sceneTitle = new Text("Welcome");
+        sceneTitle.setId("welcome-text");
+        grid.add(sceneTitle, 0, 0, 2, 1);
+
+        Label userName = new Label("User Name:");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Text actionTarget = new Text();
+        actionTarget.setId("action-target");
+        grid.add(actionTarget, 1, 6);
+        btn.setOnAction(event -> {
+            actionTarget.setText("Sign in button pressed");
+        });
+
+        Scene scene = new Scene(grid, 300, 275);
+        scene.getStylesheets().add(Main.class.getResource("/login.css").toExternalForm());
+
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
     private void tutorial2(Stage primaryStage) {
         primaryStage.setTitle("JavaFX Welcome");
 
