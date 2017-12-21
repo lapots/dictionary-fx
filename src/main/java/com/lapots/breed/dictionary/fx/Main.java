98e58b08f2f3fcd57a1d5df34a90214b28491aeb
lapots <sebastrident@gmail.com>
2017-12-21 13:32:40 +0300

    JavaFX tutorial 5 (with 4 FXML ignored)

--- src/main/java/com/lapots/breed/dictionary/fx/Main.java
+++ src/main/java/com/lapots/breed/dictionary/fx/Main.java
@@ -1,21 +1,21 @@
 package com.lapots.breed.dictionary.fx;
 
+import javafx.animation.*;
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
+import javafx.geometry.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.effect.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.*;
+import javafx.scene.shape.*;
+import javafx.scene.text.*;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.stream.IntStream;
+
+import static java.lang.Math.random;
 
 public class Main extends Application {
 
@@ -25,6 +25,63 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+    }
+
+    private void tutorial5(Stage primaryStage) {
+        Group root = new Group();
+        Scene scene = new Scene(root, 800, 600, Color.BLACK);
+        primaryStage.setScene(scene);
+
+        Rectangle colors = new Rectangle(scene.getWidth(), scene.getHeight(),
+                new LinearGradient(0f, 1f, 1f, 0f, true,
+                        CycleMethod.NO_CYCLE,
+                        new Stop(0, Color.web("#f8bd55")),
+                        new Stop(0.14, Color.web("#c0fe56")),
+                        new Stop(0.28, Color.web("#5dfbc1")),
+                        new Stop(0.43, Color.web("#64c2f8")),
+                        new Stop(0.57, Color.web("#be4af7")),
+                        new Stop(0.71, Color.web("#ed5fc2")),
+                        new Stop(0.85, Color.web("#ef504c")),
+                        new Stop(1, Color.web("#f2660f")))
+        );
+        colors.widthProperty().bind(scene.widthProperty());
+        colors.heightProperty().bind(scene.heightProperty());
+
+        Group circles = new Group();
+        circles.setEffect(new BoxBlur(10, 10, 3));
+        IntStream
+                .iterate(0, i -> i + 1)
+                .limit(30)
+                .forEach(i -> {
+                    Circle circle = new Circle(150, Color.web("white", 0.05));
+                    circle.setStrokeType(StrokeType.OUTSIDE);
+                    circle.setStroke(Color.web("white", 0.16));
+                    circle.setStrokeWidth(4);
+                    circles.getChildren().add(circle);
+                });
+
+        Group blendModeGroup = new Group(
+                new Group(
+                        new Rectangle(scene.getWidth(), scene.getHeight(), Color.BLACK), circles), colors);
+        colors.setBlendMode(BlendMode.OVERLAY);
+        root.getChildren().add(blendModeGroup);
+
+        Timeline timeline = new Timeline();
+        for (Node circle: circles.getChildren()) {
+            timeline.getKeyFrames().addAll(
+                    new KeyFrame(Duration.ZERO,
+                            new KeyValue(circle.translateXProperty(), random() * 800),
+                            new KeyValue(circle.translateYProperty(), random() * 600)
+                    ),
+                    new KeyFrame(new Duration(40000),
+                            new KeyValue(circle.translateXProperty(), random() * 800),
+                            new KeyValue(circle.translateYProperty(), random() * 600)
+                    )
+            );
+        }
+        timeline.play();
+
+        primaryStage.show();
     }
 
     private void tutorial3(Stage primaryStage) {
